960b17b31d0dd98e1e9a8993c49ac68490725fe4
Sanam1315 <152052341+Sanam1315@users.noreply.github.com>
2023-11-26 13:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function init() {
+  gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, 
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+}
+init();
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    start: "top 27%",
+    end: "top 0",
+    markers: "true",
+    scrub:3
+  }
+})
+
+// we declare anim as variable for working at same time
+tl.to(".page1 h1", {
+  x: -120,
+},"anim")
+tl.to(".page1 h2", {
+  x: 120,
+}, "anim")
+tl.to(".page1 video", {
+  width:"90%",
+},"anim")
